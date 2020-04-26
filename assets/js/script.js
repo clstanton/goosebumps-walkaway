6fcf0863ac4dee9e22962f34f16beadadc1abee4
clstanton <cstanton77@gmail.com>
2020-04-26 14:23:33 -0500

    dynamic variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-var getCityWeather = function() {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=London,us&APPID=442bae2eea7683d5465de74730b512c3");
-  };
+var getCityWeather = function(city) {
+    // format the github api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&APPID=442bae2eea7683d5465de74730b512c3";
+    
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+          });
+        });
+    };
 
-  getCityWeather();
+  getCityWeather("Austin");
