ca49c897f919fa26132a690565db45f3545b3d0c
clstanton <cstanton77@gmail.com>
2020-05-08 17:58:21 -0500

    back to working console log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,22 @@
-var weatherContainerEl = document.querySelector("#weather-container");
-var weatherSearchTerm = document.querySelector("#weather-search-term");
+// global variables
+var apiKey = "442bae2eea7683d5465de74730b512c3";
 var cityFormEl = document.querySelector("#city-form");
-var cityInputEl = document.querySelector("#cityname");
+var cityInputEl = document.querySelector("#city-name");
+var currentContainerEl = document.querySelector("#current-weather-container");
+var weatherSearchTerm = document.querySelector("#weather-search-term");
+// var searchedArr = JSON.parse(localStorage.getItem("searchedItems")) || [];
+// let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    // console.log(searchHistory);
 
+// to be executed upon a form submission browser event
 var formSubmitHandler = function(event) {
     event.preventDefault();
-
     // get value from input element
-    var cityname = cityInputEl.value.trim();
+    var city = cityInputEl.value.trim();
 
-    if (cityname) {
-        getWeather(cityname) && getForecast(cityname);
+    if (city) {
+        getCityWeather(city);
         cityInputEl.value = "";
-
       } else {
         alert("Please enter a city name!");
       }
@@ -20,29 +24,26 @@ var formSubmitHandler = function(event) {
     console.log(event);
   };
 
-var getWeather = function(city) {
-    // format the github api url
-    var apiUrlWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=442bae2eea7683d5465de74730b512c3";
-    var apiUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=442bae2eea7683d5465de74730b512c3";
-
-    // make a request to the url (help from https://gomakethings.com/waiting-for-multiple-all-api-responses-to-complete-with-the-vanilla-js-promise.all-method/)
-    Promise.all([
-        fetch(apiUrlWeather),
-        fetch(apiUrlForecast)
-    ])
-        .then(function (responses) {
-		    // Get a JSON object from each of the responses
-		    return responses.map(function (response) {
-			    return response.json();
-		    });
-	    }).then(function (data) {
-		    // Log the data to the console
-		    // You would do something with both sets of data here
- 		    console.log(data);
-        }).catch(function (error) {
-		    // if there's an error, log it
-		    console.log(error);
+// search for city weather from api
+var getCityWeather = function(city) {
+    // format the api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+          displayWeather(data, city);
+          });
         });
-    }
+};
+
+var displayWeather = function(weather, searchTerm) {
+  // clear old content
+  currentContainerEl.textContent = "";
+  weatherSearchTerm.textContent = searchTerm;
+
+  console.log(weather);
+  console.log(searchTerm);
+}
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
