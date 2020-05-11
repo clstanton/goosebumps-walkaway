d8441e395c8e7c50965eddb5e57040405a299b7c
clstanton <cstanton77@gmail.com>
2020-05-11 08:26:46 -0500

    display today's weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,39 +4,15 @@ var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city-name");
 var todaysWeatherData = document.querySelector("#todays-weather-data");
 var citySearchTerm = document.querySelector("#city-search-term");
+var todayListGroup = document.querySelector("#today-list-group");
+var todayTemp = document.querySelector("#today-temp");
+var todayHumidity = document.querySelector("#today-humidity");
+var todayWind = document.querySelector("#today-wind");
+var todayUv = document.querySelector("#today-uv");
 // var searchedArr = JSON.parse(localStorage.getItem("searchedItems")) || [];
 // let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
     // console.log(searchHistory);
 
-// to be executed upon a form submission browser event
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    // get value from input element
-    var city = cityInputEl.value.trim();
-
-    if (city) {
-        getCityWeather(city);
-        cityInputEl.value = "";
-      } else {
-        alert("Please enter a city name!");
-      }
-
-    console.log(event);
-  };
-
-// search for city weather from api
-var getCityWeather = function(city) {
-    // format the api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-    
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-          displayWeather(data, city);
-          });
-        });
-};
-  
 // today's date
 (function() {
   var now = moment().format("(l)");
@@ -46,12 +22,69 @@ var getCityWeather = function(city) {
   console.log(now);
 })();
 
-var displayWeather = function(weather, searchTerm) {
+// to be executed upon a form submission browser event
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var city = cityInputEl.value.trim();
+
+    if (city) {
+        getTodayWeather(city);
+        cityInputEl.value = "";
+      } else {
+        alert("Please enter a city name!");
+      }
+
+    console.log(event);
+  };
+
+// search for city weather from api
+var getTodayWeather = function(city) {
+    // format the api url
+    var apiUrlToday = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey;
+
+    // make a request to the url
+    fetch(apiUrlToday)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+          displayWeather(response, city);
+      })
+      .catch (function(error){
+        console.log(error);
+    });
+};
+
+// search for city weather from api
+var getForecastWeather = function(city) {
+  // format the api url
+  var apiUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey;
+    
+  // make a request to the url
+  fetch(apiUrlForecast)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      displayWeather(response, city);
+    })
+    .catch (function(error){
+      console.log(error);
+    });
+  };
+
+var displayWeather = function(response, searchTerm) {
   // clear old content
-  todaysWeatherData.textContent = "";
   citySearchTerm.textContent = searchTerm;
 
-  console.log(weather);
+  todayTemp.innerHTML = "Temperature: " + (response.main.temp);
+  todayHumidity.innerHTML = "Humidity: " + (response.main.humidity)+"%";
+  todayWind.innerHTML = "Wind Speed: " + (response.wind.speed) + " MPH";
+  todayUv.innerHTML = "UV Index: ";
+
+  console.log(todayListGroup);
+  console.log(response);
   console.log(searchTerm);
 }
 
